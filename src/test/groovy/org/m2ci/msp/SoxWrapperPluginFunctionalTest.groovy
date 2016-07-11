a9490ff543f5ac89fd0683e9fd218d2df5feb10c
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-07-11 21:26:03 +0200

    init plugin functional test

---
+++ src/test/groovy/org/m2ci/msp/SoxWrapperPluginFunctionalTest.groovy
@@ -0,0 +1,43 @@
+package org.m2ci.msp
+
+import org.gradle.testkit.runner.GradleRunner
+import org.testng.annotations.*
+
+import static org.gradle.testkit.runner.TaskOutcome.*
+
+class SoxWrapperPluginFunctionalTest {
+
+    def gradle
+
+    @BeforeClass
+    void setUp() {
+        gradle = GradleRunner.create().withPluginClasspath()
+    }
+
+    File initBuildScript() {
+        def projectDir = File.createTempDir()
+        def buildScriptFile = new File(projectDir, 'build.gradle')
+        buildScriptFile.withWriter { buildScript ->
+            buildScript << [
+                    'plugins {',
+                    '  id "org.m2ci.msp.sox-wrapper"',
+                    '}',
+                    ''
+            ].join('\n')
+        }
+        buildScriptFile
+    }
+
+    @Test
+    void canApplyPlugin() {
+        def buildScript = initBuildScript()
+        buildScript << [
+                'task test << {',
+                '  assert plugins.findPlugin("org.m2ci.msp.sox-wrapper")',
+                '}',
+                'defaultTasks "test"'
+        ].join('\n')
+        def result = gradle.withProjectDir(buildScript.parentFile).build()
+        assert result.task(':test').outcome == SUCCESS
+    }
+}
